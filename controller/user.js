862ea172a1e2ea029f3fa59d4e67493046b2c50c
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2025-04-05 17:43:40 +0530

    Update user.js

--- controller/user.js
+++ controller/user.js
@@ -22,12 +22,8 @@ exports.getViewPaper=(req,res,next)=>{
 }
 exports.getdownload=(req,res,next)=>{
     Home.findById(req.params.id).then((oneqp)=>{
-        
-        
         const imgpath=oneqp.photo.replace(/\\/g, '/');
-        console.log(imgpath);
         const imagpaths=path.join(rootDir,imgpath);
-        console.log(imagpaths);
         res.download(imagpaths);
     });
 }
