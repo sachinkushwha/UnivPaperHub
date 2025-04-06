3d370d0ab1066dc38c96b70e5e077d68f5da4426
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2025-04-06 17:45:34 +0530

    sco

--- app.js
+++ app.js
@@ -11,6 +11,49 @@ const path=require('path');
 const rootDir=require('./utils/pathutils');
 const mongoUri= "mongodb+srv://sachin:kumar@airbnb.48epv.mongodb.net/agc?retryWrites=true&w=majority&appName=airbnb";
 
+// for seo
+app.get('/sitemap.xml', (req, res) => {
+    res.setHeader('Content-Type', 'application/xml');
+    res.send(`<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://agcpyqp-1.onrender.com/</loc>
+      <priority>1.0</priority>
+    </url>
+    <url>
+      <loc>https://agcpyqp-1.onrender.com/details</loc>
+      <priority>0.8</priority>
+    </url>
+    <url>
+      <loc>https://agcpyqp-1.onrender.com/login</loc>
+      <priority>0.6</priority>
+    </url>
+    <url>
+      <loc>https://agcpyqp-1.onrender.com/register</loc>
+      <priority>0.6</priority>
+    </url>
+    <url>
+      <loc>https://agcpyqp-1.onrender.com/upload</loc>
+      <priority>0.5</priority>
+    </url>
+    <url>
+      <loc>https://agcpyqp-1.onrender.com/viewpaper/1</loc>
+      <priority>0.7</priority>
+    </url>
+    <url>
+      <loc>https://agcpyqp-1.onrender.com/download/1</loc>
+      <priority>0.7</priority>
+    </url>
+  </urlset>`);
+  });
+//for seo
+app.get('/robots.txt', (req, res) => {
+    res.type('text/plain');
+    res.send(`User-agent: *
+  Allow: /
+  Sitemap: https://agcpyqp-1.onrender.com/sitemap.xml`);
+  });
+//  
 
 const mulstorage=multer.diskStorage({
     destination:(req,file,cb)=>{
@@ -34,7 +77,7 @@ const multerOptions={
 app.use(multer(multerOptions).single('photo'));
 app.use(express.urlencoded());
 app.use('/viewpaper/uploads',express.static(path.join(rootDir,'uploads')));
-app.use(express.static(path.join(rootDir,'public')));
+// app.use(express.static(path.join(rootDir,'public')));
 app.set('view engine','ejs');
 app.set('views','views');
 
